4a70fea01fe49f0b10649aed1d176f482a6d54a0
jhrogh <wogml5031@gmail.com>
2023-12-08 17:41:34 +0900

    초기 설정

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/main/Main";
+import Join from "./pages/join/Join";
+import Ide from "./pages/ide/Ide";
+import FindInfo from "./pages/findinfo/FindInfo";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/join" element={<Join />}></Route>
+        <Route path="/ide" element={<Ide />}></Route>
+        <Route path="/findinfo" element={<FindInfo />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
