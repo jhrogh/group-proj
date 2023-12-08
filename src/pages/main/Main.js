d92f14376398e40126108743d8eac4e806a7351e
Hyejung Na <hyejung.na01@gmail.com>
2023-12-08 21:03:36 +0900

    실수

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React from 'react';
+import React from 'react';
 
 function Main() {
-  return (
-    <div>Main페이지입니다 front/jh 수정 내용</div>
-  );
+  return <div>Main페이지입니다 front/jh 수정 내용</div>;
 }
 
 export default Main;
